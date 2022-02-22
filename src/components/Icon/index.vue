fd6abfd94c444be393b6f626cbef2add1b6a23ba
Mateus Kunrath <dev.mateuskunrath@gmail.com>
2022-02-22 18:37:31 -0300

    Primeiro commit

---
+++ src/components/Icon/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <component :is="name" v-bind="$props" />
+</template>
+
+<script>
+import Loading from './Loading.vue'
+
+export default {
+  components: {
+    Loading
+  },
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: 'index',
+  props: {
+    name: { type: String, required: true }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/Credentials/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <h1>Aqui é a Credentials</h1>
+</template>
+
+<script>
+export default {
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: 'index'
+}
+</script>
+
+<style scoped>
+
+</style>
